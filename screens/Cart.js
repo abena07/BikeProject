6a0772aad94d8c90947d101a6d7461f328dc7237
abena07 <abenabennett@gmail.com>
2021-11-17 13:57:38 +0000

    cart screen in progress

--- screens/Cart.js
+++ screens/Cart.js
@@ -1,13 +1,160 @@
 
-import React from 'react';
-import {  View ,Text} from 'react-native';
+import React, {useState}from 'react';
+import {  View ,Text,StyleSheet,Image} from 'react-native';
+
+import { FlatList, TouchableOpacity } from "react-native-gesture-handler";
+import {
+  Ionicons,
+  FontAwesome,
+  AntDesign,
+  Foundation,
+  EvilIcons,
+} from "@expo/vector-icons";
+import NavBar from "../components/NavBar";
 
 export default function Cart() {
+  const [details, setDetails] = useState([
+    {
+      name: "Pinarello Bike",
+      price: "1,700.00",
+      key: "1",
+      image: require("../images/bike4.png"),
+    },
+    {
+      name: "Brompton Bike",
+      price: "1,500.00",
+      key: "2",
+      image: require("../images/bike5.png"),
+    },
+    {
+      name: "Schwin Bike",
+      price: "1,200.00",
+      key: "3",
+      image:require('../images/bike1.png'),
+    },
+    {
+      name: "Norco Bike",
+      price: "9,800.00",
+      key: "4",
+      image: require("../images/bike3.png"),
+    },
+  ]);
   return (
-    <View >
-      <Text style={{fontSize:50}}>Cart</Text>
+    <View style ={{flex:1 }}>
+      <View style={{flexDirection:"row", justifyContent:"space-evenly"}}>
+      <View style={{flexDirection:"row" , justifyContent:"flex-start"}}>
+      <Ionicons name="arrow-back" size={24} color="black" />
+      </View>
+      <View style={{fontSize:20, flexDirection:"row",justifyContent:'center', alignItems:'center'}}><Text >Cart List</Text></View>
+      </View>
+      
+      
+      
+      
+      <View style={{flexDirection:"row",  alignItems:"center", justifyContent:"center"}}>
+      <Text style ={{color:"rgb(222, 212, 221)",}}>(3 items)</Text>
+
+      </View>
+      <FlatList
+      numColumns={1}
+      
+      key={2}
+
+        data={details}
+        
+        renderItem={({ item }) => (
+          
+          <TouchableOpacity
+            onPress={() =>
+              navigation.navigate("ProductDetails", { data: item })
+            }
+          >
+            
+            <View style={styles.viewcontainer}>
+              <View style={styles.bikecontainer}>
+               
+                
+                <View>
+                  <Image source={item.image} style={styles.imagecontainer} />
+                </View>
+              
+              </View>
+
+              <View >
+              <Text style={styles.text1container}>{item.name}</Text>
+              <View style={{flexDirection:"row"}}>
+              <Foundation name="dollar" size={24} color="orange" />
+               <Text style={styles.text2container}>{item.price}</Text>
+               </View>
+              </View>
+
+  
+</View>
+            
+            
+            
+          </TouchableOpacity>
+          
+          
+        )}
+      />
+      <NavBar/>
     </View>
   );
 }
 
 
+const styles = StyleSheet.create({
+  textcontainer: {
+    backgroundColor: "rgb(211,211,211)",
+    color: "grey",
+    borderRadius: 5,
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 5,
+  },
+
+  bikecontainer: {
+    backgroundColor: "rgb(211,211,211)",
+    borderRadius: 5,
+    marginTop: 5,
+    paddingHorizontal: 5,
+    paddingVertical: 20,
+    
+    
+  },
+
+  imagecontainer: {
+    flex: 1,
+    width: 65,
+    height:50,
+    resizeMode: "contain",
+  },
+  text1container: {
+    color: "grey",
+    fontSize: 16,
+    fontWeight: "bold",
+    marginLeft:5
+  },
+  text2container: {
+    fontWeight: "bold",
+    fontSize: 18,
+    marginRight:50
+  },
+  viewcontainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginHorizontal:5,
+    marginBottom:10
+  },
+
+  heartcontainer: {
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 5,
+    position: "absolute",
+    left: 90,
+  },
+});
